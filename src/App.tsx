5a69a51ddb76f9d57af8ebf040264f15024e76bc
Sahil <hanss9545@gmail.com>
2025-07-02 01:56:41 +0530

    setup react + typescript + tailwindcss + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-screen">
+    <div className=" font-medium text-2xl">Welcome to,  <span className="text-blue-500">devBalance</span></div>
+    <Button className="bg-blue-500">Shadcn Button</Button>
+    </div>
+  )
+}
+
+export default App
