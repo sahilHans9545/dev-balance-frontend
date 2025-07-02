cb6a03c3231eeb06d332a5461179761d08e73c0f
Sahil <hanss9545@gmail.com>
2025-07-02 23:50:17 +0530

    ref #1 : added routes for signup, login and home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const Home : React.FC = () => {
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-screen">
+    <div className=" font-medium text-2xl">Welcome to,  <span className="text-blue-500">devBalance</span></div>
+    <Button className="bg-blue-500">Shadcn Button</Button>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,8 @@
+
+const Signup : React.FC = () => {
+  return (
+    <div>Welcome to Signup Page</div>
+  )
+}
+
+export default Signup
