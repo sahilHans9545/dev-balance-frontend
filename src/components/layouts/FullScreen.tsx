450ae29b41b52c95b33227ea03f8b7c3bf14d8d1
Sahil <hanss9545@gmail.com>
2025-07-11 00:04:30 +0530

    ref #5 : login page ui

--- src/components/layouts/FullScreen.tsx
+++ src/components/layouts/FullScreen.tsx
@@ -1,14 +1,13 @@
 interface FullScreenBgProps {
-    children: React.ReactNode;
-  }
-  
-const FullScreenLayout : React.FC <FullScreenBgProps> = ({children}) => {
-  return (
-    <div className="w-screen h-screen bg-gradient-to-br from-slate-50 to-blue-5 flex flex-col items-center justify-center">
-        {children}
-    </div>
-  )
+  children: React.ReactNode;
 }
 
-export default FullScreenLayout;
+const FullScreenLayout: React.FC<FullScreenBgProps> = ({ children }) => {
+  return (
+    <div className="w-screen h-screen bg-[#dbeafe52] flex flex-col items-center justify-center">
+      {children}
+    </div>
+  );
+};
 
+export default FullScreenLayout;
